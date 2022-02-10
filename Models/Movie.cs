268752195d81accf8338f003004537937d918351
mauricio <mauriciocon@gmail.com>
2022-02-10 14:55:14 -0300

    Adicionar data anotations para as propriedades Release Date e Price

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,16 +1,24 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
+        CultureInfo ptBR = new CultureInfo("pt-BR");
+
         public int Id { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        
+        // [Column(CultureInfo.GetCultureInfo)]
+        [Column(TypeName = "decimal 18, 2")]
         public decimal Price { get; set; }
     }
 }
